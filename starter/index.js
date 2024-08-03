4457944d0d42e2ef2e48380f5a96a98af4727ab3
Shuvo Sarkar <shuvosarkar5599@gmail.com>
2024-08-03 19:29:44 +0530

    read and write fiel in asyncronus way 02

--- starter/index.js
+++ starter/index.js
@@ -9,9 +9,20 @@ const fs = require("fs");
 
 // fs.writeFileSync('./txt/output.txt', txtOut);
 
-
 // non-blocking -> asyncronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
-  console.log(data);
-})
-console.log('console data!');
+
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("some error occourd!!");
+
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("Your file has been Writen.");
+      });
+    });
+  });
+});
+console.log("woll read this first!");
