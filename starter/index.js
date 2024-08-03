a3a4569d4f64d7aa76021a0a4e801c4759d91352
Shuvo Sarkar <shuvosarkar5599@gmail.com>
2024-08-03 18:43:19 +0530

    read file in asyncronous way 01

--- starter/index.js
+++ starter/index.js
@@ -1,9 +1,17 @@
 const fs = require("fs");
 
-const inptuIN = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(inptuIN);
+// // blocking -> syncronus way
+// const inptuIN = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(inptuIN);
 
-const now = new Date();
-const txtOut = `the data of (input.txt) is: ${inptuIN} and current time is: ${now.toLocaleTimeString()}`;
+// const now = new Date();
+// const txtOut = `the data of (input.txt) is: ${inptuIN} and current time is: ${now.toLocaleTimeString()}`;
 
-fs.writeFileSync('./txt/output.txt', txtOut);
+// fs.writeFileSync('./txt/output.txt', txtOut);
+
+
+// non-blocking -> asyncronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+  console.log(data);
+})
+console.log('console data!');
